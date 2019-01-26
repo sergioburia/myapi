1138e850b723b6b05cf89acef4e706f15a10cdcc
Sergio Buria <sergioburia@gmail.com>
2019-01-26 00:50:48 -0200

    Migration

--- meusite/polls/models.py
+++ meusite/polls/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date_publishe')
+    pub_date = models.DateTimeField('date_published')
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
